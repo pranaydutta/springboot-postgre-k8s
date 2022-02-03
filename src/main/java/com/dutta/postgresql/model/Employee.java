a5163cfa3fa339d4c11fb33666f3ac5915a08f88
Dutta <pranaykumardutta@gmail.com>
2022-02-03 17:23:54 +0530

    fix applcation running

--- src/main/java/com/dutta/postgresql/model/Employee.java
+++ src/main/java/com/dutta/postgresql/model/Employee.java
@@ -10,15 +10,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Data
+@Table(name = "employee")
 @Entity
 public class Employee extends EmployeeAudit{
 
-    @Id
+   
+	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
